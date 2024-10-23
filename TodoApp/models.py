2447d47b934cf5a6536d9ef2e4f14f2a2897fe52
Pawan Kalyan Akula <akulapawankalyan18@gmail.com>
2024-10-23 23:55:39 +0530

    second commit

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,17 +1,20 @@
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
+
 class Users(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique = True)
-    username = Column(String, unique = True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
     first_name = Column(String)
     last_name = Column(String)
     hashed_password = Column(String)
-    is_active = Column(Boolean, default = True)
+    is_active = Column(Boolean, default=True)
     role = Column(String)
+    phone_number = Column(String)
+
 
 class Todos(Base):
     __tablename__ = 'todos'
@@ -21,5 +24,4 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
-    owner = Column(Integer, ForeignKey("users.id"))
-   
+    owner_id = Column(Integer, ForeignKey("users.id"))
